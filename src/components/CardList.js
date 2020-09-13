643c08a83322b152a8e5c0b8fcd685d3c1588544
Haz-git <zhouharry1@gmail.com>
2020-09-13 07:45:11 -0700

    Added slight styling

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,10 +3,18 @@ import { connect } from 'react-redux';
 
 const CardList = ({ groceries }) => {
     return (
-        <div>
-            {groceries.map(element => (
-                <li key={element.id}>{element.submitNameValue}</li>
-            ))}
+        <div className='ui inverted segment'>
+            <div className='ui inverted relaxed divided list'>
+                <div className='item'>
+                    <div className='content'>
+                        <div className='header'>
+                            {groceries.map(element => (
+                                <li key={element.id}>{element.submitNameValue}</li>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
